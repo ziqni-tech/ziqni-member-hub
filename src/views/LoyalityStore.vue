d98a3fdccde7767e8bd3c571f1a0bf66e1e865a7
danilov <danilovwebb@gmail.com>
2023-01-05 16:12:10 +0200

    Added page markup Loyalty store

--- src/views/LoyalityStore.vue
+++ src/views/LoyalityStore.vue
@@ -1,10 +1,18 @@
 <template>
-  <h1 class="section-title">Loyality Store</h1>
-  <LoyalityStoreCard />
+  <div class="section-header">
+    <h1 class="section-title">Loyalty Store</h1>
+    <ActionsBlock />
+  </div>
+  <div class="tournament-cards">
+    <div v-for="(card, index) in 7" class="tournament-card-wrapper">
+      <LoyalityStoreCard :key="index"/>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import LoyalityStoreCard from '../components/LoyalityStoreCard';
+import ActionsBlock from '../shared/components/actions-block/ActionsBlock';
 
 </script>
 
