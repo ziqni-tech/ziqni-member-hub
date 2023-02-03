13bb99caf98d3c0446ecc92fbca557cc02c2bd7e
danilov <danilovwebb@gmail.com>
2023-02-03 16:39:44 +0200

    Added a table with the list of achievements

--- src/views/History.vue
+++ src/views/History.vue
@@ -1,11 +1,16 @@
 <template>
   <h1 class="section-title">History</h1>
+  <AchievementsList :achievements="achievements" />
 </template>
 
-<script>
-export default {
-  name: 'History'
-};
+<script setup>
+import AchievementsList from '../components/achievements/AchievementsList';
+import { useGetAchievements } from '../hooks/useGetAchievements';
+
+const { achievements, getAchievementsRequest } = useGetAchievements();
+
+getAchievementsRequest();
+
 </script>
 
 <style scoped>
