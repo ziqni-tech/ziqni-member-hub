002a2d6ebb77a665b971f1e9a683d6e5174cd428
danilov <danilovwebb@gmail.com>
2023-03-03 14:21:37 +0200

    Added tournaments to store

---
+++ src/store/modules/tournamentsModule.js
@@ -0,0 +1,92 @@
+const state = {
+  isLoading: false,
+  currentTournaments: [],
+  currentTournamentsTotalRecords: 0,
+  featureTournaments: [],
+  featureTournamentsTotalRecords: 0,
+  isLoaded: false,
+  error: null
+};
+const getters = {
+  getCurrentTournaments(state) {
+    return state.currentTournaments;
+  },
+  getCurrentTournamentsLoading(state) {
+    return state.isLoading
+  },
+  getCurrentTournamentsLoaded(state) {
+    return state.isLoaded
+  },
+  getCurrentTournamentsTotalRecords(state) {
+    return state.currentTournamentsTotalRecords;
+  },
+  getFeatureTournaments(state) {
+    return state.featureTournaments;
+  },
+  getFeatureTournamentsLoading(state) {
+    return state.isLoading
+  },
+  getFeatureTournamentsLoaded(state) {
+    return state.isLoaded
+  },
+  getFeatureTournamentsTotalRecords(sate) {
+    return state.featureTournamentsTotalRecords;
+  }
+};
+
+const mutations = {
+  CURRENT_TOURNAMENTS_API_REQUEST(state) {
+    state.isLoading = true;
+    state.isLoaded = false;
+  },
+  FEATURE_TOURNAMENTS_API_REQUEST(state) {
+    state.isLoading = true;
+    state.isLoaded = false;
+  },
+  SET_CURRENT_TOURNAMENTS(state, payload) {
+    const { data, meta } = payload;
+    state.currentTournamentsTotalRecords = meta.totalRecordsFound;
+    state.currentTournaments = [...state.currentTournaments, ...data];
+    state.isLoading = false;
+    state.isLoaded = true;
+  },
+  SET_FEATURE_TOURNAMENTS(state, payload) {
+    const { data, meta } = payload;
+    state.featureTournamentsTotalRecords = meta.totalRecordsFound;
+    state.featureTournaments = [...state.featureTournaments, ...data];
+    state.isLoading = false;
+    state.isLoaded = true;
+  },
+  SET_CURRENT_TOURNAMENTS_ERROR(state, payload) {
+    state.error = payload;
+    state.isLoading = false;
+    state.isLoaded = true;
+  },
+  SET_FEATURE_TOURNAMENTS_ERROR(state, payload) {
+    state.error = payload;
+    state.isLoading = false;
+    state.isLoaded = true;
+  }
+};
+
+const actions = {
+  currentTournamentsRequest({ commit }, payload) {
+    commit('CURRENT_TOURNAMENTS_API_REQUEST')
+  },
+  featureTournamentsRequest({ commit }, payload) {
+    commit('FEATURE_TOURNAMENTS_API_REQUEST')
+  },
+  setCurrentTournamentsAction({ commit }, payload) {
+    commit('SET_CURRENT_TOURNAMENTS', payload);
+  },
+  setFeatureTournamentsAction({ commit }, payload) {
+    commit('SET_FEATURE_TOURNAMENTS', payload);
+  }
+};
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions,
+};
