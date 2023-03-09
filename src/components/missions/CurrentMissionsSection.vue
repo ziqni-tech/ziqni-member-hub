8761005cb08ba2a76306affd5e12868561e71f85
danilov <danilovwebb@gmail.com>
2023-03-09 15:41:30 +0200

    Refactored Current missions section

--- src/components/missions/CurrentMissionsSection.vue
+++ src/components/missions/CurrentMissionsSection.vue
@@ -2,12 +2,12 @@
   <div class="section">
     <div class="section-header">
       <h2 class="section-title">Current Missions</h2>
-      <ActionsBlock />
+      <ActionsBlock/>
     </div>
-    <Loader v-if="isLoading" :title="'Current Missions are loading'" />
+    <Loader v-if="isLoading" :title="'Current Missions are loading'"/>
     <div class="cards-grid">
       <div class="card-wrapper" v-for="mission in currentMissions">
-        <MissionCard :mission="mission" />
+        <MissionCard :mission="mission"/>
       </div>
     </div>
     <button class="b-btn b-btn__text" v-if="currentMissions.length && isShowMore && !isDashboard" @click="loadMore">
@@ -17,13 +17,19 @@
 </template>
 
 <script setup>
-import { computed, onMounted, ref, watchEffect } from 'vue';
-import { AchievementRequest, AchievementsApiWs, ApiClientStomp } from '@ziqni-tech/member-api-client';
+import { computed, ref } from 'vue';
+import {
+  AchievementRequest,
+  AchievementsApiWs,
+  ApiClientStomp,
+  OptInApiWs,
+  OptInStatesRequest
+} from '@ziqni-tech/member-api-client';
+import { useStore } from 'vuex';
 
 import ActionsBlock from '../../shared/components/UI/actions-block/ActionsBlock';
 import MissionCard from './MissionCard';
 import Loader from '../Loader';
-import { useStore } from 'vuex';
 
 const props = defineProps({
   isDashboard: {
@@ -44,6 +50,7 @@ const totalRecords = computed(() => store.getters.getCurrentMissionsTotalRecords
 const limit = ref(3);
 const skip = ref(0);
 const isLoading = ref(false);
+const missions = ref([]);
 
 const achievementsRequest = AchievementRequest.constructFromObject({
   achievementFilter: {
@@ -71,26 +78,68 @@ const getAchievementsRequest = async () => {
   const achievementsApiWsClient = new AchievementsApiWs(ApiClientStomp.instance);
 
   await achievementsApiWsClient.getAchievements(achievementsRequest, (res) => {
-    store.dispatch('setCurrentMissionsAction', res)
+    store.dispatch('setCurrentMissionsTotalRecords', res.meta.totalRecordsFound);
+
+    const ids = res.data.map(item => item.id);
+    missions.value = res.data;
+
+    getOptInStatus(ids);
     isLoading.value = false;
   });
-}
+};
+
+const getOptInStatus = async (ids) => {
+  const optInApiWsClient = new OptInApiWs(ApiClientStomp.instance);
+
+  const optInStateRequest = OptInStatesRequest.constructFromObject({
+    optinStatesFilter: {
+      entityTypes: ['Achievement'],
+      ids: ids,
+      statusCodes: {
+        gt: 0,
+        lt: 40
+      },
+      skip: 0,
+      limit: 10
+    }
+  }, null);
+
+  await optInApiWsClient.optInStates(optInStateRequest, res => {
+    for (const mission of missions.value) {
+      if (res.data.length) {
+        const status = res.data.find(item => item.entityId === mission.id)?.status;
+        const percentage = res.data.find(item => item.entityId === mission.id)?.percentageComplete;
+
+        mission.entrantStatus = status ? status : '';
+        mission.percentageComplete = percentage ? percentage : 0;
+      } else {
+        mission.percentageComplete = 0;
+        mission.entrantStatus = '';
+      }
+    }
+    store.dispatch('setCurrentMissionsAction', missions.value);
+  });
+};
 
 if (!currentMissions.value.length) getAchievementsRequest();
 
-const isShowMore = computed(() => currentMissions.value.length < totalRecords.value)
+const isShowMore = computed(() => currentMissions.value.length < totalRecords.value);
 
-const loadMore = async() => {
+const loadMore = async () => {
   isLoading.value = true;
   const achievementsApiWsClient = new AchievementsApiWs(ApiClientStomp.instance);
 
   achievementsRequest.achievementFilter.skip = currentMissions.value.length;
 
   await achievementsApiWsClient.getAchievements(achievementsRequest, (res) => {
-    store.dispatch('setCurrentMissionsAction', res)
+    missions.value = res.data;
+
+    const ids = res.data.map(item => item.id);
+    getOptInStatus(ids);
+
     isLoading.value = false;
   });
-}
+};
 
 </script>
 
