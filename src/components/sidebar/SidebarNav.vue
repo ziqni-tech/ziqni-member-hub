7ff7266675a896d0ae32d427d76e327848347287
danilov <danilovwebb@gmail.com>
2023-05-09 12:34:39 +0300

    Sidebar refactoring

---
+++ src/components/sidebar/SidebarNav.vue
@@ -0,0 +1,69 @@
+<template>
+  <CNavItem
+      v-for="(item, index) in navItems"
+      :key="index"
+  >
+    <router-link :to="item.to" class="nav-item">
+      <div class="nav-item__icon-wrapper">
+        <component class="icon" :is="item.icon"/>
+      </div>
+      <span class="nav-item__title">{{ item.name }}</span>
+    </router-link>
+  </CNavItem>
+</template>
+
+<script setup>
+import { CNavItem } from '@coreui/vue';
+const props = defineProps({ navItems: Array });
+
+</script>
+
+<style lang="scss">
+@import 'src/assets/scss/_variables';
+
+.nav-item {
+  font-family: $mainFont;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  padding: 11px 13px;
+  border-radius: 10px;
+
+  & a {
+    text-decoration: none;
+    color: $sidebar-text-color;
+  }
+
+  & a:hover {
+    color: $main-text-color-white;
+  }
+
+  &__icon-wrapper {
+    width: 18px;
+    height: 18px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    object-fit: cover;
+    margin-right: 11px;
+
+    .icon {
+      width: inherit;
+      height: inherit;
+    }
+  }
+
+  &__title {
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 17px;
+  }
+
+  > .router-link-active {
+    background-color: $purple;
+    color: $main-text-color-white;
+  }
+}
+</style>
--- src/components/sidebar/TheSidebar.vue
+++ src/components/sidebar/TheSidebar.vue
@@ -1,33 +1,28 @@
 <template>
-  <CSidebar visible :class="isDarkMode ? 'sidebar-dark-mode' : 'sidebar-light-mode'">
+  <div id="sidebar">
     <div class="brand">
       <div class="logo-wrapper">
         <img
             class="logo"
-            :src="require(`../../assets/icons/${logoName}.svg`)"
+            :src="require(`../../assets/icons/logo-dark.svg`)"
             alt="logo"
         />
       </div>
     </div>
-    <CSidebarNav>
-      <SidebarItems :nav-items="sidebarNav"/>
-    </CSidebarNav>
-    <button @click="logOut" class="log-out-btn">
-      <Logout />
-      <span class="log-out-btn_text">Log Out</span>
-    </button>
-  </CSidebar>
+    <SidebarNav :nav-items="sidebarNav" />
+    <LogoutItem class="logout" @logOut="logOut" />
+  </div>
 </template>
 
 <script setup>
-import { CSidebar, CSidebarNav } from '@coreui/vue';
+import { CSidebar } from '@coreui/vue';
 import { useRouter } from 'vue-router';
 import { useStore } from 'vuex';
 import { computed } from 'vue';
 
-import SidebarItems from './SidebarItems';
 import sidebarNav from './sidebarNav';
-import Logout from './svg-icons/Logout';
+import SidebarNav from '@/components/sidebar/SidebarNav';
+import LogoutItem from '@/components/sidebar/LogoutItem';
 
 const router = useRouter();
 const store = useStore();
@@ -49,20 +44,29 @@ const logOut = () => emit('logOut');
 <style lang="scss">
 @import '../../assets/scss/utils/vars';
 
-.log-out-btn {
-  padding: 20px 50px;
-  text-align: start;
-  background-color: inherit;
-  outline: none;
-  border: none;
-  color: $sts-grey-medium-lighter;
+#sidebar {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
 
-  &_text {
-    margin-left: 15px;
+  .brand {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 24px 0 50px;
+
+    .logo-wrapper {
+      width: 100px;
+      height: 20px;
+    }
   }
 
-  &:hover {
-    color: $sts-grey-medium;
+
+  .logout {
+    margin-top: auto;
+    padding: 16px;
   }
 }
 </style>
