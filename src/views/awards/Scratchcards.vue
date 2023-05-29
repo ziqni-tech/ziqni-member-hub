20091f6727f43e9a32ccaa79147821037462a1fc
danilov <danilovwebb@gmail.com>
2023-05-29 15:04:04 +0300

    Added Scratchcards page

---
+++ src/views/awards/Scratchcards.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="page-content">
+    <h1 class="page-title">Scratchcards</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/scss/_variables';
+.page-content {
+  .page-title {
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 29px;
+    color: $main-text-color-white;
+    margin-top: 50px;
+  }
+
+  .page-description {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 17px;
+    color: $body-text-color;
+    padding-top: 10px;
+  }
+}
+</style>
