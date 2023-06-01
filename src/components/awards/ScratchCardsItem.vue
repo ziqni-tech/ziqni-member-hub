6c78ed40f4da9f01bd6148a0a4727ae4a6af3d61
danilov <danilovwebb@gmail.com>
2023-06-01 16:21:30 +0300

    ScratchCardsItem - added font size and auto scratch function

--- src/components/awards/ScratchCardsItem.vue
+++ src/components/awards/ScratchCardsItem.vue
@@ -1,60 +1,194 @@
 <template>
-  <div class="prize-container"></div>
-<!--  <img :src="img" alt="Prize Image" class="image" />-->
-  <img v-if="cell.scratched" :src="img" alt="Prize Image" class="image" />-->
-  <!--        <div v-else class="cover">?</div>-->
-<!--  <canvas id="scratch-card" width="80px" height="80px"></canvas>-->
+  <div class="box">
+    <canvas
+        id="myCanvas"
+        ref="canvasRef"
+        @mousedown="canvasDraw"
+        @mousemove="mouseMove"
+        @mouseup="finishDraw"
+        @touchmove="touchMove"
+        width="80"
+        height="80"
+    ></canvas>
+    <img :src="img" alt="" srcset="" width="80" height="80"/>
+  </div>
 </template>
 
 <script setup>
-import { ref, watch } from 'vue';
+import { onMounted, onUpdated, reactive, ref } from 'vue';
+import { useMouse } from '@vueuse/core';
 
 const props = defineProps({
-  img: String
-})
+  img: String,
+});
 
-const isContext = ref(false)
+const height = ref(80);
+const width = ref(80);
+const position = ref(1);
+const canvasRef = ref(null);
+const scratchText = ref('?');
 
-const generateCanvas = () => {
-  // this.canvas = document.createElement('canvas');
-  // this.canvas.classList.add('sc__canvas');
-  //
-  // // Add canvas into container
-  // this.canvas.width = this.config.containerWidth;
-  // this.canvas.height = this.config.containerHeight;
-  // this.container.appendChild(this.canvas);
-}
+const mouse = reactive(useMouse());
 
-// let canvas = document.getElementById('scratch-card')
-// let context = canvas.getContext('2d')
+onMounted(() => {
+  initCanvas();
+});
 
-const init = () => {
-  // let gradientColor = context.createLinearGradient(0,0,135,135)
-  // gradientColor.addColorStop(0,'#c3a3f1')
-  // gradientColor.addColorStop(1,'#6414e9')
-  // context.fillStyle = gradientColor
-  // context.fillRect(0,0,80,80)
-}
+onUpdated(() => {
+  initCanvas();
+});
 
-window.onload = init()
+const initCanvas = () => {
+  const canvas = canvasRef.value;
+  const ctx = canvas.getContext('2d',);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = '#1A202C';
 
-// watch(context, (value) => {
-//   if (value) console.warn('CONTEXT', value)
-// })
+  if (position.value === 1) {
+    ctx.fillRect(0, 0, height.value, width.value);
+    ctx.fillStyle = '#BEE9F3';
+    ctx.canvas.style.opacity = 1;
+
+    // font size
+    const fontSize = 40;
+    ctx.font = `${ fontSize }px Syne`;
+
+    // text position
+    const textWidth = ctx.measureText(scratchText.value).width;
+    const textX = (width.value - textWidth) / 2;
+    const textY = (height.value + fontSize) / 2;
+
+    ctx.fillText(scratchText.value, textX, textY);
+  }
+
+};
+
+const autoScratching = () => {
+  const canvas = canvasRef.value;
+  const ctx = canvas.getContext('2d',);
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+};
+
+const scratching = () => {
+  const { x, y } = getClientOffset(mouse);
+  const canvas = canvasRef.value;
+  const ctx = canvas.getContext('2d',);
+  const offsetX = x - 0.001;
+  const offsetY = y - 0.001;
+
+  ctx.globalCompositeOperation = 'destination-out';
+  ctx.lineJoin = 'round';
+  ctx.lineCap = 'round';
+  ctx.lineWidth = '30';
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.lineTo(offsetX, offsetY);
+  ctx.stroke();
+  getFilledPercentage(ctx);
+};
+
+const getClientOffset = (position) => {
+  if (canvasRef.value) {
+    const rect = canvasRef.value.getBoundingClientRect();
+
+    return {
+      x: position.x - rect.left,
+      y: position.y - rect.top,
+    };
+  }
+};
+
+const getFilledPercentage = (ctx) => {
+  let imgData;
+  if (position.value === 1) {
+    imgData = ctx.getImageData(0, 0, height.value, width.value);
+  }
+  if (position.value === 2) {
+    imgData = ctx.getImageData(150, 0, height.value, width.value);
+  }
+
+  let pixels = imgData.data;
+  let n = 0;
+  for (let i = 0; i < pixels.length; i += 100) {
+    if (pixels[i + 3] < 128) {
+      n += 100;
+    }
+  }
+
+  if (n >= pixels.length * 0.7) {
+    ctx.globalCompositeOperation = 'destination-over';
+    fadeOut(ctx);
+  }
+};
+
+const fadeOut = (ctx) => {
+  let timesRun = 0;
+  let interval = setInterval(() => {
+    timesRun += 1;
+    ctx.canvas.style.opacity *= 0.99;
+    if (timesRun === 100) {
+      clearInterval(interval);
+    }
+  }, 50);
+};
+
+let isDraw = false;
+const canvasDraw = () => {
+  isDraw = true;
+};
+
+const mouseMove = (e) => {
+  if (!isDraw) return;
+  scratching();
+};
+
+const finishDraw = () => {
+  isDraw = false;
+};
+
+const touchMove = (e) => {
+  const canvas = canvasRef.value;
+  if (e.target === canvas) {
+    e.preventDefault();
+    scratching();
+  }
+};
+
+// setTimeout(() => {
+//   console.log('autoScratching');
+//   autoScratching()
+// }, 10000);
 
 </script>
 
 <style scoped lang="scss">
-#scratch-card {
-  border: 1px solid red;
-  position: absolute;
-  top: 0;
-  left: 0;
+.box {
   width: 100%;
   height: 100%;
-  -webkit-tap-highlight-color: transparent;
-  -webkit-touch-callout: none;
-  -webkit-user-select: none;
-  user-select: none;
+  position: relative;
+  display: flex;
+  align-self: center;
+  justify-content: center;
+  background-color: #c077ee;
+}
+
+#myCanvas {
+  z-index: 2;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  cursor: url('@/assets/images/instant-wins/coin.png'), auto;
+}
+
+img {
+  position: absolute;
+  width: 90%;
+  height: 90%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
