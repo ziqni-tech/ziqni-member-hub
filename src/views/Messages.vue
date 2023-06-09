1be9abc43976e2d2a5cc515343a5b112d078cf44
danilov <danilovwebb@gmail.com>
2023-06-09 14:01:42 +0300

    Messages date and time

--- src/views/Messages.vue
+++ src/views/Messages.vue
@@ -8,7 +8,10 @@
         <span class="message-body__type" >{{ message.messageType }}</span>
         <span class="message-body__body">{{ message.body }}</span>
       </div>
-      <span class="created">{{ message.created }}</span>
+      <div class="created">
+        <span class="date">{{ formattedDate(message.created) }}</span>
+        <span class="date">{{ formattedTime(message.created) }}</span>
+      </div>
     </CListGroupItem>
   </CListGroup>
   <Loader v-else />
@@ -44,6 +47,24 @@ const getMessagesRequest = async () => {
   });
 }
 getMessagesRequest()
+
+const formattedDate = (dateString) => {
+  console.log(dateString);
+  const date = new Date(dateString)
+  let day = date.getDate();
+  let month = date.getMonth();
+  let year = date.getFullYear();
+
+  return `${day}.${month}.${year}`
+}
+
+const formattedTime = (dateString) => {
+  const date = new Date(dateString)
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+
+  return `${hours}:${minutes}`
+}
 </script>
 
 <style lang="scss" scoped>
@@ -97,11 +118,74 @@ getMessagesRequest()
   }
 
   .created {
+    display: flex;
+    flex-direction: column;
     margin-left: auto;
     font-weight: 500;
     font-size: 12px;
     line-height: 16px;
     color: $body-text-color;
+
+    .date {
+      text-align: right;
+    }
+  }
+}
+
+@media screen and (max-width: $tableWidth) {
+  .list-group-item {
+    display: flex;
+    align-items: center;
+
+    margin-bottom: 5px;
+    background-color: $light-grey;
+    color: $main-text-color-white;
+
+    .avatar {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 40px;
+      height: 40px;
+      border-radius: $border-radius-round;
+
+      & > img {
+        width: inherit;
+        height: inherit;
+      }
+    }
+
+    .message-body {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      margin-left: 10px;
+
+      &__type {
+        font-weight: 500;
+        font-size: 14px;
+        line-height: 17px;
+        color: $body-text-color;
+      }
+
+      &__body {
+        max-width: 160px;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+
+        font-weight: 400;
+        font-size: 12px;
+        line-height: 146.5%;
+        color: $main-text-color-white;
+      }
+    }
+
+    .created {
+      font-weight: 500;
+      font-size: 10px;
+      line-height: 12px;
+    }
   }
 }
 
