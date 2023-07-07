d795ed9794ed61b728897a02062cbab12924f22a
danilov <danilovwebb@gmail.com>
2023-07-07 14:23:18 +0300

    [MHP-44][ Added blazzio navigation menu ]

---
+++ src/components/sidebar/blazzioNav.js
@@ -0,0 +1,35 @@
+import Home from './svg-icons/Home';
+import Casino from './svg-icons/Casino';
+import LiveCasino from './svg-icons/LiveCasino';
+import Promotions from './svg-icons/Promotions';
+import GameStudios from './svg-icons/GameStudios';
+
+const blazzioNav = [
+    {
+        name: 'Home',
+        to: 'https://test.blazzio.com/',
+        icon: Home,
+    },
+    {
+        name: 'Casino',
+        to: 'https://test.blazzio.com/games/casino',
+        icon: Casino,
+    },
+    {
+        name: 'Live Casino',
+        to: 'https://test.blazzio.com/games/live',
+        icon: LiveCasino,
+    },
+    {
+        name: 'Promotions',
+        to: 'https://test.blazzio.com/promotions',
+        icon: Promotions,
+    },
+    {
+        name: 'Game Studios',
+        to: 'https://test.blazzio.com/studios',
+        icon: GameStudios,
+    },
+];
+
+export default blazzioNav;
